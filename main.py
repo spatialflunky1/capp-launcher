5772f164d7d7c1a5412ed3112f8887e024b539a1
Spatialflunky1 <mjandrus06@gmail.com>
2022-02-08 14:00:26 -0500

    added basic gui (needs designing)

--- main.py
+++ main.py
@@ -1,6 +1,17 @@
 import parse_files
+import tkinter as tk
+import os
 
 apps = parse_files.get_apps()
+root = tk.Tk()
+root.geometry("640x480")
+root.title("Cool Application Launcher")
+
+app_buttons = []
 
 for app in apps:
-    print(app["Type"])
+    if app["Type"] == "Application" and "Exec" in app:
+        app_name = tk.Button(text=app["Name"], command=lambda run = app["Exec"]: os.system(run))
+        app_name.pack(anchor="w")
+        app_buttons.append(app_name)
+root.mainloop()
